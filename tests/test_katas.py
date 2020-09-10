c74926aec4e478377fcede79439191076f5d5be5
Marcel Cornett <marcornett@gmail.com>
2020-09-10 09:32:56 -0400

    Completed tests

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -1,22 +1,32 @@
 import unittest
-import katas
+from katas import *
 
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(-1, -1), 1)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(power(2, 2), 4)
+        self.assertEqual(power(10, 4), 10000)
+        self.assertEqual(power(6, 8), 1679616)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        self.assertEqual(factorial(4), 24)
+        self.assertEqual(factorial(7), 5040)
+        self.assertEqual(factorial(5), 120)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        self.assertEqual(fibonacci(6), 5)
+        self.assertEqual(fibonacci(8), 13)
+        self.assertEqual(fibonacci(10), 34)
 
 
 if __name__ == '__main__':
